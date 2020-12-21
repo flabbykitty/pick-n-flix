041f8f19ffc4432ec1388617ecb142e278aa3d93
Jasmine Silfver <jasmine.silfver@gmail.com>
2020-12-21 17:00:25 +0100

    Added react router and components to home and login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react'
 import './assets/App.scss'
 import Navigation from './components/Navigation'
+import {Switch, Route} from 'react-router-dom'
+import Login from './components/pages/Login'
+import Home from './components/pages/Home'
+
 
 const App = () => {
-  return (
-    <div>
-      <Navigation/>
-    </div>
-  )
+	return (
+		<div>
+			<Navigation/>
+
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+
+					<Route path="/login">
+						<Login />
+					</Route>
+				</Switch>
+		</div>
+	)
 }
 
 export default App
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import {Container, Nav, Navbar} from 'react-bootstrap'
+import {NavLink, Link} from 'react-router-dom'
 
 const Navigation = () => {
     return (
         <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
-                <Navbar.Brand href="#">Pick 'n' Flix</Navbar.Brand>
+                <Link to="/">Pick 'n' Flix</Link>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
-                        <Nav.Link href="#link">Login</Nav.Link>
+                        <NavLink to="/login">Login</NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
