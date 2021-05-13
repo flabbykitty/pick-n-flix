df76c5f8b0e4e3fca49f457426f48ee2cfed895e
Jasmine Silfver <jasmine.silfver@gmail.com>
2021-05-13 14:35:27 +0200

    only render search results if there is a query

--- src/components/pages/SearchResults.js
+++ src/components/pages/SearchResults.js
@@ -51,89 +51,101 @@ const SearchResults = () => {
     return (
         <div id="search-results">
             <SearchBar/>
-			<Form className="d-flex">
-				<Form.Group controlId="selectYearFrom">
-					<Form.Label>Year from</Form.Label>
-					<Form.Control as="select" custom onChange={e => setYearFrom(e.target.value)}>
-					{yearRange.map(s => {
-						return s
-					})}
-					</Form.Control>
-				</Form.Group>
-				<Form.Group controlId="selectYearTo">
-					<Form.Label>Year to</Form.Label>
-					<Form.Control as="select" custom onChange={e => setYearTo(e.target.value)}>
-					{yearRange.map(s => {
-						return s
-					})}
-					</Form.Control>
-				</Form.Group>
-			</Form>
-			
-            <h1>{data.total_results} {data.total_results === 1 ? 'result' : 'results'} for '{query}'</h1>
 
-			{data.total_results > 0 && (
+			{query && (
 				<>
-            	<p>Page {data.page} out of {data.total_pages}</p>
-				<div className="d-flex mb-3 justify-content-between">
-					<Button
-						onClick={() => setPage(prevPage => (data.page === 1)
-								? prevPage
-								: prevPage - 1)}
-						disabled={data.page === 1}
-						className="btn btn-outline-secondary">Previous Page</Button>
+				<Form className="d-flex">
+					<Form.Group controlId="selectYearFrom">
+						<Form.Label>Year from</Form.Label>
+						<Form.Control as="select" custom onChange={e => setYearFrom(e.target.value)}>
+						{yearRange.map(s => {
+							return s
+						})}
+						</Form.Control>
+					</Form.Group>
+					<Form.Group controlId="selectYearTo">
+						<Form.Label>Year to</Form.Label>
+						<Form.Control as="select" custom onChange={e => setYearTo(e.target.value)}>
+						{yearRange.map(s => {
+							return s
+						})}
+						</Form.Control>
+					</Form.Group>
+				</Form>
 
-					<Button
-						onClick={() => setPage(prevPage => (data.page === data.total_pages)
-							? prevPage
-							: prevPage + 1)}
-						disabled={data.page === data.total_pages || data.total_pages === 0}
-						className="btn btn-outline-secondary">Next Page</Button>
-				</div>
+            	<h1>{data.total_results} {data.total_results === 1 ? 'result' : 'results'} for '{query}'</h1>
+
+				{data.total_results > 0 && (
+					<>
+					<p>Page {data.page} out of {data.total_pages}</p>
+					<div className="d-flex mb-3 justify-content-between">
+						<Button
+							onClick={() => setPage(prevPage => (data.page === 1)
+									? prevPage
+									: prevPage - 1)}
+							disabled={data.page === 1}
+							className="btn btn-outline-secondary">Previous Page</Button>
+
+						<Button
+							onClick={() => setPage(prevPage => (data.page === data.total_pages)
+								? prevPage
+								: prevPage + 1)}
+							disabled={data.page === data.total_pages || data.total_pages === 0}
+							className="btn btn-outline-secondary">Next Page</Button>
+					</div>
+					</>
+				)}
+
+				{data.results && (
+					<>
+						<div className="grid-container">
+							{yearFrom && !yearTo && data.results.filter(m => m.release_date.substring(0,4) >= yearFrom).map((movie, index) => (
+								<MovieItem key={index} movie={movie} />
+							))}
+
+							{yearTo && !yearFrom && data.results.filter(m => yearTo >= m.release_date.substring(0,4)).map((movie, index) => (
+								<MovieItem key={index} movie={movie} />
+							))}
+
+							{yearFrom && yearTo && data.results.filter(m => yearTo >= m.release_date.substring(0,4) && m.release_date.substring(0,4) >= yearFrom).map((movie, index) => (
+								<MovieItem key={index} movie={movie} />
+							))}
+
+							{(!yearFrom && !yearTo) && data.results.map((movie, index) => (
+								<MovieItem key={index} movie={movie} />
+							))}
+						</div>
+					</>
+				)}
+
+				{data.total_results > 0 && (
+					<div className="d-flex mb-3 justify-content-between align-items-baseline text-center">
+						<Button
+							onClick={() => setPage(prevPage => (data.page === 1)
+									? prevPage
+									: prevPage - 1)}
+							disabled={data.page === 1}
+							className="btn btn-outline-secondary">Previous Page</Button>
+
+						<p>Page {data.page} out of {data.total_pages}</p>
+
+						<Button
+							onClick={() => setPage(prevPage => (data.page === data.total_pages)
+								? prevPage
+								: prevPage + 1)}
+							disabled={data.page === data.total_pages || data.total_pages === 0}
+							className="btn btn-outline-secondary">Next Page</Button>
+					</div>
+				)}
+
 				</>
 			)}
 
-            {data.results && (
-                <>
-                    <div className="grid-container">
-						{yearFrom && !yearTo && data.results.filter(m => m.release_date.substring(0,4) >= yearFrom).map((movie, index) => (
-                            <MovieItem key={index} movie={movie} />
-                        ))}
 
-						{yearTo && !yearFrom && data.results.filter(m => yearTo >= m.release_date.substring(0,4)).map((movie, index) => (
-                            <MovieItem key={index} movie={movie} />
-                        ))}
+			
 
-						{yearFrom && yearTo && data.results.filter(m => yearTo >= m.release_date.substring(0,4) && m.release_date.substring(0,4) >= yearFrom).map((movie, index) => (
-                            <MovieItem key={index} movie={movie} />
-                        ))}
 
-                        {(!yearFrom && !yearTo) && data.results.map((movie, index) => (
-                            <MovieItem key={index} movie={movie} />
-                        ))}
-                    </div>
-                </>
-            )}
 
-            {data.total_results > 0 && (
-				<div className="d-flex mb-3 justify-content-between align-items-baseline text-center">
-					<Button
-						onClick={() => setPage(prevPage => (data.page === 1)
-								? prevPage
-								: prevPage - 1)}
-						disabled={data.page === 1}
-						className="btn btn-outline-secondary">Previous Page</Button>
-
-					<p>Page {data.page} out of {data.total_pages}</p>
-
-					<Button
-						onClick={() => setPage(prevPage => (data.page === data.total_pages)
-							? prevPage
-							: prevPage + 1)}
-						disabled={data.page === data.total_pages || data.total_pages === 0}
-						className="btn btn-outline-secondary">Next Page</Button>
-				</div>
-			)}
 		</div>
     )
 }
